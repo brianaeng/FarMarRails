e902ffc4c63ac78c3e851ac148941dcd83aa6f25
Briana Eng <brianaeng@gmail.com>
2016-10-04 22:12:44 -0700

    Added methods for market controller

--- app/controllers/market_controller.rb
+++ app/controllers/market_controller.rb
@@ -1,22 +1,54 @@
 class MarketController < ApplicationController
   def index
+    @all_markets = Market.all
   end
 
   def show
+    find
   end
 
   def new
+    @market = Market.new
   end
 
   def create
+    @market = Market.new
+    #do name, address, etc. need to be strings?
+    @market.name = params[:market][:name]
+    @market.address = params[:market][:address]
+    @market.city = params[:market][:city]
+    @market.county = params[:market][:county]
+    @market.state = params[:market][:state]
+    @market.zip = params[:market][:zip]
+    @market.save
+
+    # redirect_to root_path
   end
 
   def edit
+    find
   end
 
   def update
+    find
+
+    @market.name = params[:market][:name]
+    @market.address = params[:market][:address]
+    @market.city = params[:market][:city]
+    @market.county = params[:market][:county]
+    @market.state = params[:market][:state]
+    @market.zip = params[:market][:zip]
+    @market.save
   end
 
   def destroy
+    find
+    @market.destroy
+  end
+
+private
+  def find
+    @market = Market.find(params[:id].to_i)
+    #self.class.find(params[:id].to_i)
   end
 end
