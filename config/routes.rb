cad8e3c18286e647a4df1eb5e17068afd873801a
Alyssa Hursh <akhursh@gmail.com>
2016-10-06 13:42:50 -0700

    Search bar functions for vendor product and market

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get '/product_search', to: 'pages#product_search', as: 'product_search'
 
+  get '/market_search', to: 'pages#market_search', as: 'market_search'
+
   get '/home', to: 'pages#home', as: 'home'
 
   get '/admin', to: 'pages#admin', as: 'admin'
