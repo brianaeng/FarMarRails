fa59cc0b96b7ecf5d7c361935a1d632604d9bc32
Alyssa Hursh <akhursh@gmail.com>
2016-10-07 10:48:10 -0700

    Removed ids from database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,17 +15,17 @@ CSV.foreach('seed_csvs/markets.csv', :headers => false) do |market|
     zip = "0#{zip}"
   end
 
-  Market.create(id: market[0].to_i, name: market[1], address: market[2], city: market[3], county: market[4], state: market[5], zip: zip)
+  Market.create(name: market[1], address: market[2], city: market[3], county: market[4], state: market[5], zip: zip)
 end
 
 CSV.foreach('seed_csvs/vendors.csv', :headers => false) do |vendor|
-  Vendor.create(id: vendor[0].to_i, name: vendor[1], num_of_employees: vendor[2].to_i, market_id: vendor[3].to_i)
+  Vendor.create(name: vendor[1], num_of_employees: vendor[2].to_i, market_id: vendor[3].to_i)
 end
 
 CSV.foreach('seed_csvs/sales.csv', :headers => false) do |sale|
-  Sale.create(id: sale[0].to_i, amount: sale[1].to_i, purchase_time: DateTime.parse(sale[2]), vendor_id: sale[3].to_i, product_id: sale[4].to_i)
+  Sale.create(amount: sale[1].to_i, purchase_time: DateTime.parse(sale[2]), vendor_id: sale[3].to_i, product_id: sale[4].to_i)
 end
 
 CSV.foreach('seed_csvs/products.csv', :headers => false) do |product|
-  Product.create(id: product[0].to_i, name: product[1], vendor_id: product[2].to_i)
+  Product.create(name: product[1], vendor_id: product[2].to_i)
 end
